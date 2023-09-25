816e9dadf2c69b17288bcf22c7d3404407bc1c25
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-09-25 13:38:27 -0400

    Get single product endpoint

--- index.js
+++ index.js
@@ -22,17 +22,16 @@ server.listen(PORT, HOST, function () {
 	console.log('----------------------------');
 	console.log(`   GET PRODUCTS (method: GET) => ${HOST}:${PORT}/products`);
 	console.log(
-		`   GET SINGLE PRODUCT (method: GET) => ${HOST}:${PORT}/product/:id`
+		`   GET SINGLE PRODUCT (method: GET) => ${HOST}:${PORT}/products/:id`
 	);
 	console.log(
-		`   DELETE A PRODUCT (method: DELETE) => ${HOST}:${PORT}/product/:id`
+		`   DELETE A PRODUCT (method: DELETE) => ${HOST}:${PORT}/products/:id`
 	);
 	console.log(`   ADD A PRODUCT (method: POST) => ${HOST}:${PORT}/products`);
 });
 
 // Get all products in the system
 server.get('/products', function (req, res, next) {
-
 	//Increment the get count
 	getCount += 1;
 
@@ -41,12 +40,43 @@ server.get('/products', function (req, res, next) {
 		`Processed Request Count--> Get: ${getCount}, Post:${postCount}, Delete: ${deleteCount}`
 	);
 	console.log('GET /products params=>' + JSON.stringify(req.params));
-	console.log('> products GET: received request');
+	console.log('>get all products GET: received request');
 
-	// Find every entity within the given collection
+	// Find every product within the given collection
 	productMockDataBase.find({}, function (error, products) {
-		console.log('< products GET: sending response');
+		console.log('< get all products GET: sending response');
 		// Return all of the products in the system
 		res.send(products);
 	});
+});
+
+// Get a single product by their product id
+server.get('/products/:id', function (req, res, next) {
+	//Increment the get count
+	getCount += 1;
+
+	//Log the count and endpoint info
+	console.log(
+		`Processed Request Count--> Get: ${getCount}, Post:${postCount}, Delete: ${deleteCount}`
+	);
+	console.log('GET /products/:id params=>' + JSON.stringify(req.params));
+	console.log('>get a single product GET: received request');
+
+	// Find a single product by their id within the mock database
+	productMockDataBase.findOne(
+		{ _id: req.params.id },
+		function (error, product) {
+			// If there are any errors, pass them to next in the correct format
+			if (error) return next(new Error(JSON.stringify(error.errors)));
+
+			if (product) {
+				console.log('<get a single product GET: sending response');
+				// Send the product if no issues
+				res.send(product);
+			} else {
+				// Send 404 header if the user doesn't exist
+				res.send(404);
+			}
+		}
+	);
 });
